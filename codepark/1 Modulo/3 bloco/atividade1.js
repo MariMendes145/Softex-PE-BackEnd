85b30f0de40732694fc569d406d92654dc33176d
Marianne Mendes <mendes.mariannedev@gmail.com>
2023-08-02 10:23:10 -0300

    novas atividades bloco3

---
+++ codepark/1 Modulo/3 bloco/atividade1.js
@@ -0,0 +1,25 @@
+/* Você deve criar três listas em JavaScript:
+
+1. para guardar os nomes de dez pessoas;
+2. para guardar as suas idades;
+3. para guardar as suas cores favoritas.
+
+Em seguida, imprima essas listas. Depois, faça algumas modificações, alterando uma cor e uma idade. Ao terminar, imprima novamente todas as informações das três listas. */
+
+const { array } = require("blockly/core/utils");
+
+let nome = [
+  "Amanda",
+  "Beatriz",
+  "Carlos",
+  "Diego",
+  "João",
+  "José",
+  "Marianne",
+  "Mirian",
+  "Rodrigo",
+  "Saulo",
+  "Pedro",
+];
+console.log(nome);
+let idade = [];
